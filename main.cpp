09b6051ce8ed0e9b69988430d12c56805cf3e315
FluxWisdom <86100619+FluxWisdom@users.noreply.github.com>
2024-08-11 11:15:12 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -14,8 +14,25 @@ APP_VERSION("1.0.2")
 // Example for triangle(x0,y0,x1,y1,x2,y2,colorFill,colorLine);
 // Example for line(x1,y1,x2,y2,color);
 
+class vec3 {
+public:
+  float x;
+  float z;
+  float y;
+};
+
+// typedef unsigned vec3;
+
+float sqrt(float num) { return num; };
+
+float length(vec3 v) { return sqrt((v.x * v.x) + (v.y * v.y) + (v.z * v.z)); };
+
+float sdSphere(vec3 p, float s) { return length(p) - s; };
+
 extern int width;
 extern int height;
+
+uint16_t RayMarch(float x, float y) { return color(255, 255, 255); }
 
 extern "C" void main() {
   calcInit(); // backup screen and init some variables
@@ -24,7 +41,8 @@ extern "C" void main() {
 
   for (int w = 1; w <= width; w++) {
     for (int h = 1; h <= height; h++) {
-      LCD_SetPixel(w, h, color(255, 255, 255));
+
+      LCD_SetPixel(w, h, RayMarch(w, h));
     };
   };
 
